b48de46e8d7ebd32de8df902ac08e5e3b4687620
DRIRU <47264999+DRIRU@users.noreply.github.com>
2023-03-28 18:37:56 +0300

    Update transportation.py

--- transportation.py
+++ transportation.py
@@ -7,14 +7,29 @@ for i in range(r):
     for j in range(c):
         row.append(int(input()))
     transTable.append(row)
+
 print("Enter the supply values: ")
 supply = []
 for i in range(r):
     supply.append(int(input()))
 print("Enter the demand values: ")
 demand = []
-for i in range(r):
+for i in range(c):
     demand.append(int(input()))
-if sum(supply) != sum(supply):
-    if(sum(supply) > sum(supply)):
-        
+
+if sum(supply) != sum(demand):
+    if(sum(supply) < sum(demand)):
+        demand.append(sum(demand)-sum(supply))
+        row = []
+        for i in range(c):
+            row.append(0)
+        transTable.append(row)
+        r += 1
+    else:
+        supply.append(sum(supply)-sum(demand))
+        for i in range(r):
+            transTable[i].append(0)
+        c += 1
+print(transTable)
+print(supply)
+print(demand)
