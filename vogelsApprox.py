395dc077e9b6d87fa5ba53196a21f14cb0aea66b
DRIRU <47264999+DRIRU@users.noreply.github.com>
2023-03-30 13:50:14 +0300

    Update vogelsApprox.py

--- vogelsApprox.py
+++ vogelsApprox.py
@@ -4,7 +4,10 @@ def setPenalty(tb):
     for i in range(len(tb)):
         row = sorted(set(tb[i]))
         row = list(row)
-        min1, min2 = row[0], row[1]
+        if len(row) == 1 and row[0] == 0:
+            min1, min2 = 0 , 0
+        else:
+            min1, min2 = row[0], row[1]
         rowPen.append(min2-min1)
     for j in range(len(tb[0])):
         col = []
@@ -12,24 +15,43 @@ def setPenalty(tb):
             col.append(tb[i][j])
         col = sorted(set(col))
         col = list(col)
-        min1, min2 = col[0], col[1]
+        if len(col) == 1 and col[0] == 0:
+            min1, min2 = 0 , 0
+        else:
+            min1, min2 = col[0], col[1]
         colPen.append(min2-min1)
     return rowPen, colPen
-
+def findMinColIndex(tb, i):
+    mini = 1000
+    j = 0
+    for n in range(len(tb[0])):
+        if tb[i][n] < mini and tb[i][n] != 0:
+            mini = tb[i][n]
+            j = n
+        return j
+def findMinRowIndex(tb, j):
+    mini = 1000
+    i = 0
+    for n in range(len(tb)):
+        if tb[n][j] < mini and tb[n][j] != 0:
+            mini = tb[n][j]
+            i = n
+        return i
 def findIndex(row, col, tb):
     if len(tb) < 2 or len(tb[0]) < 2:
-        
-    if max(row) >= max(col):
+        if len(tb) < 2:
+            print(tb)
+            i = 0
+            j = findMinColIndex(tb, i)
+        else:
+            j = 0
+            i = findMinRowIndex(tb, j)
+    elif max(row) >= max(col):
         i = row.index(max(row))
-        j = row.index(min(tb[i]))
+        j = findMinColIndex(tb, i)
     else:
         j = col.index(max(col))
-        i = 0
-        mini = tb[i][j]
-        for n in range(len(tb)): 
-            if tb[n][j] < mini:
-                mini = tb[n][j]
-                i = n
+        i = findMinRowIndex(tb, j)
     return i, j
 
 r = int(input("Enter Row and Column size of Transportation table: "))
@@ -66,30 +88,27 @@ opTable, sol = transTable, 0
 while True:
     if len(opTable) == 0:
         break
-    if len(opTable) >= 2 or len(opTable[0]) >= 2: 
+    if len(opTable) >= 2 and len(opTable[0]) >= 2: 
         rowPen, colPen = setPenalty(opTable)
-        rowIndex, colIndex = findIndex(rowPen, colPen, opTable)
-    else:
-        rowIndex = 0
-        colIndex = 0
+    rowIndex, colIndex = findIndex(rowPen, colPen, opTable)
     if supply[rowIndex] == demand[colIndex]:
-        sol += demand[colIndex] * transTable[rowIndex][colIndex]
-        print("%d * %d"%(demand[colIndex],transTable[rowIndex][colIndex]))
+        sol += demand[colIndex] * opTable[rowIndex][colIndex]
+        print("%d * %d"%(demand[colIndex], opTable[rowIndex][colIndex]))
         demand.pop(colIndex)
         supply.pop(rowIndex)
         opTable.pop(rowIndex)
         for i in range(len(opTable)):
             opTable[i].pop(colIndex)
     elif supply[rowIndex] > demand[colIndex]:
-        sol += demand[colIndex] * transTable[rowIndex][colIndex]
-        print("%d * %d"%(demand[colIndex],transTable[rowIndex][colIndex]))
+        sol += demand[colIndex] * opTable[rowIndex][colIndex]
+        print("%d * %d"%(demand[colIndex],opTable[rowIndex][colIndex]))
         supply[rowIndex] -= demand[colIndex]
         demand.pop(colIndex) 
         for i in range(len(opTable)):
             opTable[i].pop(colIndex)
     else:
-        sol += supply[rowIndex] * transTable[rowIndex][colIndex]
-        print("%d * %d"%(supply[rowIndex],transTable[rowIndex][colIndex]))
+        sol += supply[rowIndex] * opTable[rowIndex][colIndex]
+        print("%d * %d"%(supply[rowIndex],opTable[rowIndex][colIndex]))
         demand[colIndex] -= supply[rowIndex]
         supply.pop(rowIndex)
         opTable.pop(rowIndex)
