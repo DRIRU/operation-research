aeeeb0ae888242f3b05d7680ecf38d3556246b0b
DRIRU <47264999+DRIRU@users.noreply.github.com>
2023-03-30 15:01:11 +0300

    Update main.py
    added vogel's approximation

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from transportation import*
 while True:
-    ch = int(input("1. North West Corner Method\n2. Least Cost Method\n3. Exit\nEnter Choice: "))
+    ch = int(input("\n1. North West Corner Method\n2. Least Cost Method\n3. Vogel's Approximation Method\n0. Exit\nEnter Choice: "))
+    if ch == 0:
+        exit()
     table, supply, demand = insertValues()
     supply, demand, table = checkBalance(supply, demand, table)
     if ch == 1:
         northWest(table, supply, demand)
     elif ch == 2:
         leastCost(table, supply, demand)
-    elif ch == 3:
-        exit()
+    elif ch ==3:
+        vogelsApprox(table, supply, demand)
     else:
         print("Wrong Input")
