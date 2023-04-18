049564829289024fe4172a7e61864b22d548765a
DRIRU <47264999+DRIRU@users.noreply.github.com>
2023-04-18 22:31:59 +0530

    Create finalAssign.py

---
+++ finalAssign.py
@@ -0,0 +1,91 @@
+def leastCost(opTable, supply, demand):
+    sol = 0
+    leastCostElement,minRow, minCol = findMin(opTable)
+    while True:
+        if len(opTable) == 0:
+            break
+        leastCostElement,minRow, minCol = findMin(opTable)
+        if supply[minRow] == demand[minCol]:
+            sol += demand[minCol] * opTable[minRow][minCol]
+            demand.pop(minCol)
+            supply.pop(minRow)
+            opTable.pop(minRow)
+            for i in range(len(opTable)):
+                opTable[i].pop(minCol)
+        elif supply[minRow] > demand[minCol]:
+            sol += demand[minCol] * opTable[minRow][minCol]
+            supply[minRow] -= demand[minCol]
+            demand.pop(minCol)
+            for i in range(len(opTable)):
+                opTable[i].pop(minCol)
+        else:
+            sol += supply[minRow] * opTable[minRow][minCol]
+            demand[minCol] -= supply[minRow]
+            supply.pop(minRow)
+            opTable.pop(minRow)
+    print("Initial feasible solution is %d"%sol)
+def northWest(transTable, supply, demand):
+    i, j, sol = 0, 0, 0
+    while i != len(transTable) and j != len(transTable[0]):
+        if supply[i] == demand[j]:
+            sol += demand[j] * transTable[i][j]
+            demand[j],supply[i], i, j = 0, 0, (i + 1), (j + 1)
+        elif supply[i] > demand[j]:
+            sol += demand[j] * transTable[i][j]
+            supply[i] -= demand[j]
+            demand[j], j = 0, (j + 1)
+        else:
+            sol += supply[i] * transTable[i][j]
+            demand[j] -= supply[i]
+            supply[i], i = 0, (i + 1)
+    print("Initial feasible solution is %d"%sol)
+def insertValues():
+    r, c = int(input("Enter Row and Column size of Transportation table: ")), int(input())
+    print("Enter Elements: ")
+    transTable = []
+    for i in range(r):
+        row = []
+        for j in range(c):
+            row.append(int(input()))
+        transTable.append(row)
+    print("Enter the supply values: ")
+    supply = []
+    for i in range(r):
+        supply.append(int(input()))
+    print("Enter the demand values: ")
+    demand = []
+    for i in range(c):
+        demand.append(int(input()))
+    return transTable, supply, demand
+def checkBalance(supply, demand, transTable):
+    if sum(supply) != sum(demand):
+        if(sum(supply) > sum(demand)):
+            demand.append(sum(supply)-sum(demand))
+            for i in range(len(transTable)):
+                transTable[i].append(0)
+        else:
+            supply.append(sum(demand)-sum(supply))
+            row = []
+            for i in range(len(transTable[0])):
+                row.append(0)
+            transTable.append(row)
+    return supply, demand, transTable
+def findMin(tab):
+    minValue, minColIndex, minRowIndex = tab[0][0], 0, 0
+    for i in range(len(tab)):
+        for j in range(len(tab[0])):
+            if tab[i][j] < minValue and tab[i][j] != 0:
+                minValue, minRowIndex, minColIndex = tab[i][j], i, j
+    return minValue, minRowIndex, minColIndex
+while True:
+    ch = int(input("\n1. North West Corner Method\n2. Least Cost Method\n0. Exit\nEnter Choice: "))
+    if ch == 0:
+        exit()
+    table, supply, demand = insertValues()
+    supply, demand, table = checkBalance(supply, demand, table)
+    if ch == 1:
+        northWest(table, supply, demand)
+    elif ch == 2:
+        leastCost(table, supply, demand)
+    else:
+        print("Wrong Input")
